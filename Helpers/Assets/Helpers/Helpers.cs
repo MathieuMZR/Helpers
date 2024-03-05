4b373251aee2e76088f2fdf22a2bef7dcf4463a3
MathieuMZR <m.meziere@rubika-edu.com>
2024-03-05 15:12:01 +0100

    First Function Test

---
+++ Helpers/Assets/Helpers/Helpers.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Helpers
+{
+    public static float OneOrZero()
+    {
+        return Random.Range(0, 2);
+    }
+}
